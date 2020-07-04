1adb9f58ad3a970862ec71484dfde1acc2ba2d55
Railton98 <railtonleal98@gmail.com>
2020-07-03 23:14:59 -0300

    remove debug banner

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -5,6 +5,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter GoFast',
       theme: ThemeData(
         primarySwatch: Colors.orange,
